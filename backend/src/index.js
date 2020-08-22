e627dc0f124b45e86e4578bc32522aab087a312d
Akul Baiju <akulbaiju@Akuls-MacBook-Air.local>
2020-08-23 00:31:38 +0530

    (#1) Setup Basic Express server

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const middlewares = require('./middlewares');
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Hello Miss Angular Web Developer!",
+    })
+})
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 1337;
+app.listen(port, () => {
+    console.log("Open 'http://localhost:1337");
+});
