9440d344cc51f4da8e3b1049bfddf81950f824d6
Akul Baiju <abhay0198@gmail.com>
2020-08-25 18:37:03 +0530

    (#6) Added google calendar sync to booking route

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,8 +1,8 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "Shambhavi",
+  user: "akulbaiju",
+  password: "25jan2001",
   host: "localhost",
   port: 5432,
   database: "dental"
--- backend/src/index.js
+++ backend/src/index.js
@@ -30,10 +30,63 @@ app.post("/book", async(req, res) => {
       "INSERT INTO bookings (patient_name, contact, gender, appointment, description) VALUES ($1 , $2 , $3 , $4 , $5) RETURNING *",
       [name, contact, gender, appointment, description]
     );
-    res.json(newBooking.rows[0]); //returns readable object
+    res.json("Accepted"); //returns accepted status code
+    //need to set start and end time from appointment
+    // Can also change appointment structure into month day and hour fields
+    var event = {
+      'summary': name,
+      'description': description,
+      'start': {
+        'dateTime': '2020-08-25T09:00:00',
+        'timeZone': 'Asia/Kolkata',
+      },
+      'end': {
+        'dateTime': '2020-08-25T09:30:00',
+        'timeZone': 'Asia/Kolkata',
+      },
+      'reminders': {
+        'useDefault': false,
+        'overrides': [
+          {'method': 'email', 'minutes': 24 * 60},
+          {'method': 'popup', 'minutes': 10},
+        ],
+      },
+    };
+    
+    calendar.events.insert({
+      auth: jwtClient,
+      calendarId: 'abhay0198@gmail.com',
+      resource: event,
+    }, function(err, event) {
+      if (err) {
+        console.log('There was an error contacting the Calendar service: ' + err);
+        return;
+      }
+      console.log('Event created: %s', event.data.htmlLink);
+    });
+   /* calendar.events.list({
+       auth: jwtClient,
+       calendarId: 'abhay0198@gmail.com'
+    }, function (err, response) {
+       if (err) {
+           console.log('The API returned an error: ' + err);
+           return;
+       }
+       console.log(response);
+       var events = response.data.items;
+       if (events.length == 0) {
+           console.log('No events found.');
+       } else {
+           console.log('Event from Google Calendar:');
+           for (let event of response.data.items) {
+               console.log('Event name: %s', event.summary);
+           }
+       }
+    });*/
     //console.log(req.body); //Test on POSTMAN! I didn't do :p
   } catch (err) {
     console.error(err.message);
+  }
 });
 
 //edit(update) bookings?
@@ -50,11 +103,39 @@ app.get('/admin', async(req, res) => {
 
     try {
       //Select already gives you back data so no need for "returning *"
-      const allPayments = await pool.query("SELECT * FROM payments");
+      const allPayments = await pool.query("SELECT * FROM bookings");
       res.json(allPayments.rows);
     } catch (err) {
       console.error(err.message);
     }
+})
+
+let {google} = require('googleapis');
+let privatekey = require("./privatekey.json");
+// configure a JWT auth client
+let jwtClient = new google.auth.JWT(
+  privatekey.client_email,
+  null,
+  privatekey.private_key,
+  ['https://www.googleapis.com/auth/calendar']);
+//authenticate request
+jwtClient.authorize(function (err, tokens) {
+if (err) {
+console.log(err);
+return;
+} else {
+console.log("Successfully connected!");
+}
+});
+
+let calendar = google.calendar('v3');
+
+
+
+app.get('/', (req, res) => {
+  res.json({
+    message: "Hii",
+  })
 })
 
 app.use(middlewares.notFound);
