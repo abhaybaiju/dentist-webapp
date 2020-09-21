b1f3a3abf6be24ab4e68a2b1d7937e52d3d6a28a
Shambhavi <sarin.shambhavi@gmail.com>
2020-09-22 01:37:25 +0530

    (#11) Date and Time

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {MuiThemeProvider, createMuiTheme, Container, Grid, makeStyles, Stepper, Step, StepButton, StepLabel, Button, Typography, StepIcon} from '@material-ui/core';
+import {Table, TableRow, TableCell, Card, CardHeader, CardContent, Container, Grid, makeStyles, Stepper, Step, StepButton, StepLabel, Button, Typography, StepIcon} from '@material-ui/core';
 import Navbar from './Navbar';
 import {Redirect } from 'react-router-dom';
+import DateFnsUtils from '@date-io/date-fns';
+import {DatePicker, MuiPickersUtilsProvider} from '@material-ui/pickers';
 
 const Process = () => {
   const useStyles = makeStyles((theme) => ({
@@ -10,45 +12,35 @@ const Process = () => {
   },
   button: {
     marginRight: theme.spacing(1),
-    backgroundColor: '#ff7a59',
-    color:'white',
+    backgroundColor: 'white',
+    color:'#ff7a59',
+    border:'1px solid #ff7a59',
     "&:hover": {
-      backgroundColor: "#ff7a59"
+      backgroundColor: "#ff7a59",
+      color:'white'
     }
   },
   step: {
     textAlign:'center',
     width:'100%',
-    border:'1px solid grey',
-    borderRadius:'50px',
-    padding:'0.5%'
-  },
-  completestep: {
-    textAlign:'center',
-    width:'100%',
-    border:'1px solid #ff7a59',
-    borderRadius:'50px',
-    backgroundColor:'#ff7a59',
     padding:'0.5%'
   },
   completed: {
     display: 'inline-block',
   },
   instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
   },
   icon:{
-    color: '#ff7a59',
+    color: 'grey',
     paddingLeft:'50%',
     "&$activeIcon": {
-      fill:'white',
+      fill:'#ff7a59',
       '& $text': {
-        fill: '#ff7a59',
+        fill: '#white',
       },
     },
     "&$completedIcon": {
-      color: 'white'
+      color: '#ff7a59'
     }
   },
   activeIcon: {},
@@ -68,17 +60,76 @@ const Process = () => {
   completedLabel: {}
 }));
 
+const One = () => {
+  const classes = useStyles();
+  const [am, setAM] = React.useState(true);
+  const sup="AM";
+  const pm="PM";
+  const morning = [['11:00', '11:15', '11:30'], ['11:45', '12:00', '12:15'], ['12:30', '12:45', '01:00']]
+  const evening = [['06:30', '06:45', '07:00'], ['07:15', '07:30', '07:45'], ['08:00', '08:15', '08:30']]
+  return(
+    <Grid style={{marginRight:'5%', marginLeft:'5%'}}>
+      <Grid container justify="center" alignItems="baseline" spacing={10}>
+        <Grid item lg={6}>
+          <h3>Pick your date</h3>
+        </Grid>
+        <Grid item lg={6}>
+          <h3>Pick your time</h3>
+        </Grid>
+      </Grid>
+      <Grid container justify="space-evenly" alignItems="baseline" spacing={10}>
+        <Grid item lg={6}>
+          <Card variant="outlined">
+            <CardContent style={{marginLeft:'14%', marginTop:'5%'}}>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <DatePicker disableToolbar autoOk variant="static" openTo="date" value={date} onChange={changeDate}/>
+              </MuiPickersUtilsProvider>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item lg={6}>
+          <Card variant="outlined" style={{width:'100%'}}>
+            <CardHeader>
+
+            </CardHeader>
+            <CardContent>
+            <Button variant="contained" className={classes.button} onClick={() => setAM(true)}>AM</Button>
+            <Button variant="contained" className={classes.button} onClick={() => setAM(false)}>PM</Button>
+              <Table >
+                {(am)?morning.map((timeList,i) =>(
+                   <TableRow key={i} >
+                    {timeList.map((time,j)=>
+                         <TableCell key={j}>{time}<sup>{(time==='11:00'||time==='11:15'||time==='11:30'||time==='11:45')?sup:pm}</sup></TableCell>
+                    )}
+                   </TableRow>
+                ))
+                : evening.map((timeList,i) =>(
+                   <TableRow key={i}>
+                    {timeList.map((time,j)=>
+                         <TableCell key={j}>{time}<sup>{pm}</sup></TableCell>
+                    )}
+                   </TableRow>
+                ))}
+              </Table>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
 function getSteps() {
   return ['Confirm date and time', 'Patient Details', 'Confirm your appointment'];
 }
+
+const [date, changeDate] = React.useState(new Date());
 
 function getStepContent(step) {
   switch (step) {
     case 0:
       return (
-        <Grid>
-          Step 1: Select campaign settings...
-        </Grid>
+        <One />
       );
     case 1:
       return (
@@ -137,17 +188,15 @@ function HorizontalNonLinearStepper() {
   return (
     <Grid>
       <Grid>
-      <MuiThemeProvider>
         <Stepper nonLinear activeStep={activeStep} connector={false}>
             {steps.map((label, index) => (
-                <Step key={label} className={(activeStep === index || completed[index]) ? classes.completestep : classes.step}>
+                <Step key={label} className={classes.step}>
                   <StepLabel completed={completed[index]}  StepIconProps={{ classes:{ root: classes.icon, active: classes.activeIcon, completed: classes.completedIcon } }}>
                     <p >{label}</p>
                   </StepLabel>
                 </Step>
             ))}
         </Stepper>
-      </MuiThemeProvider>
       </Grid>
       <div>
         {allStepsCompleted() ? (
@@ -173,9 +222,11 @@ function HorizontalNonLinearStepper() {
     </Grid>
   );
 }
+
+
 return(
   <HorizontalNonLinearStepper />
-)
+);
 }
 
 const Book = () => {
