c2d2b8b051a821b03d57dfb44e4c8cc8978ca81b
Abhay Baiju <abhay0198@gmail.com>
2020-09-25 09:49:35 +0530

    Refactored Grids

--- frontend/src/components/FrontPage.js
+++ frontend/src/components/FrontPage.js
@@ -1,28 +1,22 @@
 import React from 'react';
-import {Container, Grid} from '@material-ui/core';
+import {Container, Grid,Button} from '@material-ui/core';
 import Navbar from './Navbar';
 
 const FrontPage = () => {
   return(
     <Container>
-      <Grid>
-        <Grid>
-          <Navbar/>
-        </Grid>
-        <Grid container alignItems="center" justify="space-evenly" style={{background:'#d3eaf0', marginTop:'8%', borderRadius:'50px'}}>
+      <Navbar />
+      <Grid spacing={7} direction="row" container alignItems="center" justify="center" style={{ minHeight: '100vh'}}>
           <Grid item lg={4} style={{color:'#027e97'}}>
             <h1 style={{lineHeight:'1.3'}}>Booking appointments made easy</h1><br/>
             <Grid item lg={8}>
               <p style={{lineHeight:'1.6'}}>Hassle-free booking for you at your convenient time. Hassle-free booking for you at your convenient time.</p>
             </Grid>
+            <Button variant="outlined">Book Now</Button>
           </Grid>
           <Grid item lg={5}>
             <img alt="..." src={require('./images/doctors.svg')} height="400"/>
           </Grid>
-        </Grid>
-        <Grid>
-      {/*FrontPage*/}
-        </Grid>
       </Grid>
     </Container>
   );
