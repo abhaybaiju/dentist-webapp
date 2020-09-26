207c89820217da6aef45ca16360668748bb40488
Shambhavi <sarin.shambhavi@gmail.com>
2020-09-27 03:37:20 +0530

    (#16) Form designed

--- frontend/src/components/ComponentTwo.js
+++ frontend/src/components/ComponentTwo.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Table, TableRow, TableCell, Card, CardHeader, CardContent, Grid, makeStyles, Button, TextField} from '@material-ui/core';
+import {Table, TableRow, TableCell, Card, CardHeader, CardContent, Grid, Button, TextField, InputLabel, Select, MenuItem, FormControl, FormLabel, FormControlLabel, RadioGroup, makeStyles, Radio} from '@material-ui/core';
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 import { MuiThemeProvider } from '@material-ui/core'
 import { createMuiTheme } from '@material-ui/core/styles';
@@ -9,14 +9,17 @@ const ComponentTwo = () => {
   const theme = createMuiTheme({
     palette: {
   		primary: {
-  			main: '#027e97',
+  			main: '#ced1d8',
   		}
   	},
     overrides: {
         MuiOutlinedInput: {
             root: {
+                '& fieldset': {
+                  borderColor:'#ced1d8',
+                },
                 '&:hover:not($disabled):not($focused):not($error) $notchedOutline': {
-                borderColor: '#ff7a59',
+                borderColor: '#ced1d8',
             },
         },
       }
@@ -24,12 +27,14 @@ const ComponentTwo = () => {
   });
 
   const useStyles = makeStyles((theme) => ({
-  cell: {
-    "&:hover": {
-      borderColor:'#ff7a59'
-    }
-  },
-}));
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
 
 const Two = () => {
   const classes = useStyles();
@@ -37,34 +42,52 @@ const Two = () => {
   const [contact, setContact] = useState('');
   const [gender, setGender] = useState('');
   const [age, setAge] = useState('');
+  let array = Array.from({length: 100}, (_, i) => i + 1)
+  const lesser = "<1";
+  const greater = ">100";
 
   return(
     <Grid container justify="center" alignItems="stretch" >
 
         <Grid item lg={8}>
-          <Card variant="outlined" style={{marginTop:'10%', textAlign:'center', borderRadius:'0px', border:'20px solid #e1e2e4', background:'linear-gradient(to top left, #436375 30%, #586e80 50%, #a9b7c0 50%)', backgroundRepeat:'no-repeat'}}>
+          <Card variant="outlined" style={{marginTop:'10%', textAlign:'center', borderRadius:'0px', border:'20px solid #e1e2e4', background:'linear-gradient(to top left, #7e909e 10%, #436375 30%, #586e80 , #a9b7c0 90%)', backgroundRepeat:'no-repeat'}}>
             <CardContent>
-              <form className={classes.root} noValidate autoComplete="off" style={{marginLeft:'5%', marginTop:'5.5%', marginBottom:'3.4%'}}>
+              <form noValidate autoComplete="off" style={{marginLeft:'5%', marginTop:'5.5%', marginBottom:'3.4%', overflow:'hidden'}}>
                 <Table>
                   <TableRow>
                     <MuiThemeProvider theme={theme}>
-                      <TableCell style={{borderBottom:'none'}}><TextField id="name" label="Your name" variant="outlined" color="primary"/></TableCell>
-                      <TableCell style={{borderBottom:'none'}}><TextField id="name" label="Your name" variant="outlined" color="primary"/></TableCell>
+                      <TableCell style={{borderBottom:'none', width:'100%'}}><TextField id="name" label="Your name" variant="outlined" color="primary" fullWidth={true}/></TableCell>
                     </MuiThemeProvider>
                   </TableRow>
                   <TableRow>
                     <MuiThemeProvider theme={theme}>
-                      <TableCell style={{borderBottom:'none'}}><TextField id="name" label="Your name" variant="outlined" color="primary"/></TableCell>
-                      <TableCell style={{borderBottom:'none'}}><TextField id="name" label="Your contact" variant="outlined" color="primary"/></TableCell>
+                      <TableCell style={{borderBottom:'none', width:'100%'}}><TextField id="name" label="Your phone number" variant="outlined" color="primary" fullWidth={true}/></TableCell>
                     </MuiThemeProvider>
                   </TableRow>
+
                   <TableRow>
                     <MuiThemeProvider theme={theme}>
-                      <TableCell style={{borderBottom:'none'}}>
-                        <TextField id="name" label="Your age" variant="outlined" color="primary" className={classes.cell}/>
-                      </TableCell>
-                      <TableCell style={{borderBottom:'none'}}>
-                        <TextField id="name" label="Your gender" variant="outlined" color="primary"/>
+                      <TableCell style={{borderBottom:'none', width:'100%'}}>
+                        <div style={{float:'left', width:'55%'}}>
+                          <FormLabel component="legend">Gender</FormLabel>
+                          <RadioGroup aria-label="gender" name="gender" value={gender} onChange={(e)=>setGender(e.target.value)} row>
+                            <FormControlLabel value="Male" control={<Radio />} label="Male" />
+                            <FormControlLabel value="Female" control={<Radio />} label="Female" />
+                            <FormControlLabel value="Others" control={<Radio />} label="Others" />
+                          </RadioGroup>
+                        </div>
+                        <div style={{float:'right', width:'45%', marginRight:'-18%', marginTop:'-2%'}}>
+                          <FormControl className={classes.formControl}>
+                          <InputLabel id="ageLabel">Age</InputLabel>
+                          <Select labelId="age" id="age" value={age} onChange={(e)=>setAge(e.target.value)}>
+                            <MenuItem value={lesser}>{lesser}</MenuItem>
+                            {array.map((key,value) => (
+                              <MenuItem value={value+1}>{value+1}</MenuItem>
+                            ))}
+                            <MenuItem value={greater}>{greater}</MenuItem>
+                          </Select>
+                          </FormControl>
+                        </div>
                       </TableCell>
                     </MuiThemeProvider>
                   </TableRow>
