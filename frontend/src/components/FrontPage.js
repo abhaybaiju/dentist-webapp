e177494ae533ce747f6ecccb6340a7173bd13fbc
Abhay Baiju <abhay0198@gmail.com>
2020-09-27 13:20:57 +0530

    Styled button and added timed fade

--- frontend/src/components/FrontPage.js
+++ frontend/src/components/FrontPage.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import {Container, Grid,Button} from '@material-ui/core';
+import {Container, Grid,Button, makeStyles, Fade} from '@material-ui/core';
 import Navbar from './Navbar';
 import {Link} from 'react-router-dom';
 import './css/FrontPage.css';
 
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    background: 'linear-gradient(to right, #027e97 0%, #63a6b7  51%, #027e97 100%)',
+    color: 'white',
+    transition: '0.5s',
+    boxShadow: '0 0 20px #eee',
+    borderRadius: '10px',
+    padding: '10px 60px',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    backgroundSize: '200% auto',
+    display: 'block',
+    "&:hover": {
+      backgroundPosition: 'right center', /* change the direction of the change here */
+      color: '#fff',
+      textDecoration:'none'
+    }
+  }
+}));
+
+
+
 const FrontPage = () => {
+
+  const classes = useStyles();
+
   return(
+    <Fade in={true} style={{ transformOrigin: '0 0 0' }}
+          {...(true ? { timeout: 900 } : {})}>
     <Container>
       <div class="custom-shape-divider-bottom-1601099806">
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
         <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
     </svg>
 </div>
+
       <Navbar />
       <Grid spacing={7} direction="row" container alignItems="center" justify="center" style={{ minHeight: '100vh'}}>
           <Grid item lg={4} style={{color:'#027e97'}}>
@@ -19,13 +48,15 @@ const FrontPage = () => {
             <Grid item lg={8}>
               <p style={{lineHeight:'1.6'}}>Hassle-free booking for you at your convenient time. Hassle-free booking for you at your convenient time.</p>
             </Grid>
-            <Link to="/Book"><Button variant="outlined">Book Now</Button></Link>
+            <Link to="/Book"><Button className={classes.button} variant="outlined">Book Now</Button></Link>
           </Grid>
           <Grid item lg={5}>
             <img alt="..." src={require('./images/doctors.svg')} height="400"/>
           </Grid>
       </Grid>
     </Container>
+    </Fade>
+
   );
 }
 
