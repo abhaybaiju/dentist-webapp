7986a1ab3a4043de2cff119a247895e57dc268c3
Abhay Baiju <abhay0198@gmail.com>
2020-09-27 14:35:47 +0530

    Added background on scroll

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,40 +1,72 @@
 import React from 'react';
-import {Grid, AppBar, Toolbar, Typography} from '@material-ui/core';
+import {Grid, AppBar, Toolbar, Typography, makeStyles} from '@material-ui/core';
 import './css/Navbar.css';
 import {Link} from 'react-router-dom';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
 const Navbar = () => {
+
+  const useStyles = makeStyles((theme) => ({
+    appBar: {
+      backgroundColor: 'transparent',
+      color:'grey', 
+      boxShadow: 'none',
+      transition: theme.transitions.create(['background-color'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.standard
+      }),
+      padding: '0 20px',
+    },
+    appBarScrolled: {
+      backgroundColor: '#317e96',
+      color:'white', 
+      boxShadow: 'none',
+      transition: theme.transitions.create(['background-color'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.standard
+      })
+    },
+    toolbarTitle: {
+      flexGrow: 1
+    },
+    toolbar: {
+      flexWrap: 'wrap'
+    },
+    toolbarLogo: {
+      flexGrow: 1
+    },
+    link: {
+      margin: theme.spacing(1, 1.5)
+    }
+  }));
+
+  const classes = useStyles();
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 50
+  });
+
   return(
     <Grid>
-      <AppBar title="Dental-app" style={{backgroundColor:'transparent', color:'grey', boxShadow: 'none'}}>
+      <AppBar title="Dental-app" className={`${classes.appBar} ${
+          trigger === false ? '' : classes.appBarScrolled
+        }`}>
         <Toolbar style={{ marginLeft:'10%', marginRight:'10%', marginTop:'1%'}}>
           <Grid container justify="flex-start" alignItems="center">
             <Typography>
               <Link to="/"><img alt="..." src={require("./images/logo.png")} height="50" style={{transform: 'rotate(-90deg)', cursor:'pointer'}}/></Link>
             </Typography>
-            <Typography style={{marginLeft:'3%', color: 'grey', fontWeight:'bold'}}>
+            <Typography style={{marginLeft:'3%', fontWeight:'bold'}}>
                 LILA DENTAL CLINIC
             </Typography>
           </Grid>
           <Grid container justify="space-evenly" spacing={3}>
-            <Grid item>
               <a href="/">Home</a>
-            </Grid>
-            <Grid item>
               <a href="/">About Us</a>
-            </Grid>
-            <Grid item>
               <a href="/">Services</a>
-            </Grid>
-            <Grid item>
               <a href="/">Our Specialists</a>
-            </Grid>
-            <Grid item>
               <a href="/">Blog</a>
-            </Grid>
-            <Grid item>
               <a href="/">Contact Us</a>
-            </Grid>
           </Grid>
         </Toolbar>
       </AppBar>
