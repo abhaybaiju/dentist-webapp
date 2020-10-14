3ba60974d0c4b3b406914f118d7cf6b8387c95f2
Shambhavi <sarin.shambhavi@gmail.com>
2020-10-14 13:54:32 +0530

    (#46) Form

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -220,21 +220,12 @@ const Process = () => {
 const VerticalStepper = () => {
 
   const useStyles = makeStyles((theme) => ({
-    root: {
-      width: '100%',
-    },
     button: {
-      marginTop: theme.spacing(2),
-      marginRight: theme.spacing(1),
-      paddingTop: theme.spacing(2),
-      paddingBottom: theme.spacing(2),
-    },
-    actionsContainer: {
-      marginBottom: theme.spacing(2),
+      textAlign:'center'
     },
     connectorActive: {
      '& $line': {
-       animation:`$mymove 1.5s ${theme.transitions.easing.easeIn}`,
+       animation:`$mymove 1s ${theme.transitions.easing.easeIn}`,
        animationFillMode:'forwards',
        // animationDelay:'3s'
      },
@@ -257,7 +248,23 @@ const VerticalStepper = () => {
      "100%": {
        borderLeft:'3px solid white'
      }
-   }
+   },
+   icon:{
+     // color: '#3f3d56',
+     // paddingLeft:'50%',
+     fontSize:'35px',
+     "&$activeIcon": {
+       fill:'#3f3d56',
+       '& $text': {
+         fill: 'white',
+       },
+     },
+     "&$completedIcon": {
+       color: 'white'
+     }
+   },
+   activeIcon: {},
+   completedIcon: {}
   }));
 
   //const [check, setCheck] = useState(true);
@@ -279,44 +286,62 @@ const VerticalStepper = () => {
     }
   }
   const classes = useStyles();
+  const [{globalName, globalEmail, globalPhone, globalGender, globalAge, globalTime}, dispatch] = useStateValue();
   const [activeStep, setActiveStep] = React.useState(0);
+  const [completed, setCompleted] = React.useState({});
   const steps = getSteps();
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const allStepsCompleted = () => {
+    return completed[2]&&completed[1]&&completed[0];
   };
 
   const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    setActiveStep(prevActiveStep => (prevActiveStep-1));
+    const newCompleted = completed;
+    newCompleted[activeStep] = false;
+    setCompleted(newCompleted);
+  };
+
+  const handleNext = () => {
+    const newCompleted = completed;
+    if(activeStep === 1){
+      console.log(activeStep);
+      if(globalName === null || globalEmail === null|| globalPhone === null|| globalGender === null|| globalAge === null){
+        alert("Please fill all required fields");
+      }
+      else{
+        console.log("All filled out");
+        newCompleted[activeStep] = true;
+        setCompleted(newCompleted);
+        setActiveStep(prevActiveStep => (prevActiveStep+1));
+      }
+    } else{
+      newCompleted[activeStep] = true;
+      setCompleted(newCompleted);
+      setActiveStep(prevActiveStep => (prevActiveStep+1));
+    }
   };
 
   return (
     <Grid container alignItems="stretch" justify="center" style={{ marginLeft:'5%', marginRight:'5%', width:'90%'}}>
-      <Grid item lg={3} style={{background:'linear-gradient(to right, #1c92d2, #a5d6f3 70%, #d0f4fb 90%)', border:'1px solid #1c92d2'}}>
-        <Stepper activeStep={activeStep} orientation="vertical" connector={<StepConnector classes={{active:classes.connectorActive, completed:classes.connectorCompleted, line:classes.line}}/>} style={{margin:'20% 5%', background:'transparent', height:200}}>
+      <Grid item lg={3} style={{background:'#1c92d2', border:'1px solid #1c92d2', height:570}}>
+        <Stepper activeStep={activeStep} orientation="vertical" connector={<StepConnector classes={{active:classes.connectorActive, completed:classes.connectorCompleted, line:classes.line}}/>} style={{margin:'10% 5%', background:'transparent', height:200}}>
           {steps.map((label, index) => (
             <Step key={label}>
-              <StepLabel>{label}</StepLabel>
+              <StepLabel StepIconProps={{ classes:{ root: classes.icon, active: classes.activeIcon, completed: classes.completedIcon } }}>
+                <p style={(completed[index] || activeStep===index)?{color:'white', fontSize:'18px'}:{fontSize:'17px'}}>{label}</p>
+              </StepLabel>
             </Step>
           ))}
         </Stepper>
       </Grid>
-      <Grid item lg={8} style={{border:'1px solid #3f3d56', borderLeft:'none'}}>
-        <Slide direction="left" in={true} mountOnEnter unmountOnExit timeout={{appear: 500, enter: 300, exit: 500,}}>
-          <div>
-            <Typography style={{margin:'5% 0%'}}>{getStepContent(activeStep)}</Typography>
-            <div className={classes.actionsContainer}>
-              <div style={{textAlign:'center'}}>
-                <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button} style={{float:'left', display:'none'}}>
-                  Back
-                </Button>
-                <Button variant="contained" color="primary" onClick={handleNext} className={classes.button} style={(activeStep === steps.length - 1)?{display:'none'}:{borderRadius:'50%'}}>
-                  <ExpandMoreIcon style={{fontSize:'30px', margin:'0'}}/>
-                </Button>
-              </div>
-            </div>
-          </div>
-        </Slide>
+      <Grid item lg={8} style={{border:'1px solid #3f3d56', borderLeft:'none', height:570}}>
+        <Typography style={{margin:'5% 0%'}}>{getStepContent(activeStep)}</Typography>
+        <div style={{textAlign:'center', marginBottom:'2%'}}>
+        <Button color="primary" onClick={handleNext} style={(activeStep === steps.length - 1)?{display:'none'}:{ textAlign:'center'}}>
+          <ExpandMoreIcon style={{fontSize:'40px', border:'1px solid #3f3d56', color:'white', background:'#3f3d56', borderRadius:'50%'}}/>
+        </Button>
+        </div>
       </Grid>
     </Grid>
   );
@@ -325,7 +350,7 @@ const VerticalStepper = () => {
 const Book = () => {
   return(
     <Grid style={{overflowX:'hidden'}}>
-        <Grid container alignItems="center" justify="space-evenly" style={{background:'linear-gradient(to right, #f2fcfe, #1c92d2)', paddingBottom:'15%'}}>
+        <Grid container alignItems="center" justify="space-evenly" style={{background:'linear-gradient(to right, #f2fcfe, #1c92d2)', paddingBottom:'10%'}}>
           <Grid item lg={12} style={{marginBottom:'8%'}}>
             <Navbar/>
           </Grid>
@@ -349,3 +374,5 @@ const Book = () => {
 }
 
 export default Book;
+
+// linear-gradient(to right, #1c92d2, #a5d6f3 70%, #d0f4fb 90%)
--- frontend/src/components/InputForm.js
+++ frontend/src/components/InputForm.js
@@ -3,15 +3,220 @@ import {Table, TableRow, TableCell, Grid, TextField, InputLabel, Select, MenuIte
 import { MuiThemeProvider } from '@material-ui/core'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import PhoneEnabledIcon from '@material-ui/icons/PhoneEnabled';
-import ContactMailIcon from '@material-ui/icons/ContactMail';
 import EmailIcon from '@material-ui/icons/Email';
 import { useStateValue } from './StateProvider.js';
 import { createMuiTheme } from '@material-ui/core/styles';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+import ClearIcon from '@material-ui/icons/Clear';
+import DoneIcon from '@material-ui/icons/Done';
+
+import {Input, OutlinedInput, InputAdornment, IconButton} from '@material-ui/core';
 
 
 const InputForm = () => {
+
+  /*const theme = createMuiTheme({
+    palette: {
+  		primary: {
+  			main: '#027e97',
+  		}
+  	},
+    overrides: {
+        MuiTextField: {
+            root: {
+                borderColor:'#ff7a59',
+                '& fieldset': {
+                  borderColor:'#ff7a59',
+                },
+                '&:hover:not($disabled):not($focused):not($error) $notchedOutline': {
+                borderColor: '#ff7a59',
+            },
+        },
+      }
+    }
+  });
+
+  const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    field: {
+      '&:hover': {
+        borderColor: '#ff7a59'
+      },
+      '&$focus': {
+        borderColor: '#ff7a59'
+      },
+      '&$notchedOutline': {
+        borderColor:'#ff7a59!important',
+      }
+    },
+    hover: {},
+    notchedOutline: {},
+    menu: {
+      '&:hover': {
+        backgroundColor: '#ff7a59',
+        color:'white',
+      },
+      '&$focused': {
+        backgroundColor: '#ff7a59',
+        color:'white',
+      }
+    },
+    focused: {},
+    radio: {
+      '&$checked': {
+        color: '#ff7a59'
+      }
+    },
+    checked: {}
+  }));
+
+
+  const classes = useStyles();*/
+  const [{globalName, globalEmail, globalPhone, globalGender, globalAge}, dispatch] = useStateValue();
+
+  const [name, setName] = useState(globalName);
+  const [email, setEmail] = useState(globalEmail);
+  const [phone, setPhone] = useState(globalPhone);
+  const [gender, setGender] = useState(globalGender);
+  const [age, setAge] = useState(globalAge);
+  const [validname, setValidName] = useState("");
+  const [validemail, setValidEmail] = useState("");
+  const [validphone, setValidPhone] = useState("");
+
+  let array = Array.from({length: 100}, (_, i) => i + 1)
+  const lesser = "<1";
+  const greater = ">100";
+
+
+  const handleAgeChange = (val) => {
+    setAge(val);
+    if(name ==="" || email===""|| phone===""|| gender==="" || validname!=true || validemail!=true || validphone!=true){
+      console.log("Fields are empty");
+      console.log(name,email,phone,gender,"hello");
+      return;
+    }
+    dispatch({
+      type: 'SET_FORM',
+      globalName: name,
+      globalEmail: email,
+      globalPhone: phone,
+      globalGender: gender,
+      globalAge: val,
+    });
+  }
+
+  return(
+    <Grid container justify="center" alignItems="center">
+      <Grid item lg={12}>
+        <h2 style={{color:'#2f2b4f', textAlign:'center'}}><AccountCircleIcon style={{marginRight:'2%', marginBottom:'-1.5%'}}/>Personal Details</h2>
+      </Grid>
+      <Grid item lg={10}>
+        <FormControl variant="outlined" style={{width:'100%', margin:'2% 0%'}}>
+          <InputLabel htmlFor="name">Full name</InputLabel>
+          <OutlinedInput required id="name" type={'text'} value={name?name: globalName} placeholder="Your name" color="primary"
+            onChange={(e)=>{
+              setName(e.target.value);
+              e.target.value.match("^[a-zA-Z ]*$")===null? setValidName(false) : setValidName(true);
+            }}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton edge="end" style={validname?{color:'green'}:{color:'red'}}>
+                  {validname===""? "": validname ? <DoneIcon /> : <ClearIcon/>}
+                </IconButton>
+              </InputAdornment>
+            }
+            startAdornment={
+              <InputAdornment position="start">
+                <IconButton edge="start" style={{color:'#3f3d56'}}>
+                  <AccountCircleIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={70}
+          />
+        </FormControl>
+      </Grid>
+      <Grid item lg={10}>
+        <FormControl variant="outlined" style={{width:'100%', margin:'2% 0%'}}>
+          <InputLabel htmlFor="email">Email</InputLabel>
+          <OutlinedInput required id="email" type={'text'} value={email?email: globalEmail} placeholder="Your email id" color="primary"
+            onChange={(e)=>{
+              setEmail(e.target.value);
+              e.target.value.match("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")===null? setValidEmail(false) : setValidEmail(true);
+            }}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton edge="end" style={validemail?{color:'green'}:{color:'red'}}>
+                  {validemail===""? "": validemail ? <DoneIcon /> : <ClearIcon/>}
+                </IconButton>
+              </InputAdornment>
+            }
+            startAdornment={
+              <InputAdornment position="start">
+                <IconButton edge="start" style={{color:'#3f3d56'}}>
+                  <EmailIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={40}
+          />
+        </FormControl>
+      </Grid>
+      <Grid item lg={10}>
+        <FormControl variant="outlined" style={{width:'100%', margin:'2% 0%'}}>
+          <InputLabel htmlFor="phone">Contact</InputLabel>
+          <OutlinedInput required id="phone" type={'text'} value={phone?phone: globalPhone} placeholder="Your phone number" color="primary"
+            onChange={(e)=>{
+              setPhone(e.target.value);
+              e.target.value.match("^[0-9]{10}$")===null? setValidPhone(false) : setValidPhone(true);
+            }}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton edge="end" style={validphone?{color:'green'}:{color:'red'}}>
+                  {validphone===""? "": validphone ? <DoneIcon /> : <ClearIcon/>}
+                </IconButton>
+              </InputAdornment>
+            }
+            startAdornment={
+              <InputAdornment position="start">
+                <IconButton edge="start" style={{color:'#3f3d56'}}>
+                  <PhoneEnabledIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={55}
+          />
+        </FormControl>
+      </Grid>
+      <Grid item lg={5}>
+        <FormControl component="fieldset" style={{margin:'2% 0%'}}>
+          <FormLabel component="legend" style={{color:'grey'}}>Gender</FormLabel>
+          <RadioGroup row aria-label="gender" name="gender" value={gender?gender: globalGender} onChange={(e)=>setGender(e.target.value)}>
+            <FormControlLabel value="Male" control={<Radio style={{color:'#3f3d56'}} />} label="Male" />
+            <FormControlLabel value="Female" control={<Radio style={{color:'#3f3d56'}} />} label="Female" />
+            <FormControlLabel value="Others" control={<Radio style={{color:'#3f3d56'}} />} label="Others" />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Grid item lg={5}>
+        <FormControl component="fieldset" style={{margin:'2% 0%'}}>
+          <FormLabel component="legend" style={{color:'grey'}}>Gender</FormLabel>
+          <RadioGroup row aria-label="gender" name="gender" >
+            <FormControlLabel value="Male" control={<Radio style={{color:'#3f3d56'}} />} label="Male" />
+            <FormControlLabel value="Female" control={<Radio style={{color:'#3f3d56'}} />} label="Female" />
+            <FormControlLabel value="Others" control={<Radio style={{color:'#3f3d56'}} />} label="Others" />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+    </Grid>
+  )
+}
+
+
+/*const InputForm = () => {
 
   const theme = createMuiTheme({
     palette: {
@@ -160,7 +365,7 @@ const InputForm = () => {
                       <PhoneEnabledIcon style={{color:'#ff7a59'}}/>
                     </Grid>
                     <Grid item lg={10}>
-                      <TextField required id="phone" value={phone?phone: globalPhone} label="Your phone number" 
+                      <TextField required id="phone" value={phone?phone: globalPhone} label="Your phone number"
                       classes={{root: classes.field, hover:classes.hover, notchedOutline: classes.notchedOutline}}
                       onChange={(e)=>{
                         setPhone(e.target.value);
@@ -205,6 +410,6 @@ const InputForm = () => {
       </Grid>
     </Grid>
   );
-}
+}*/
 
 export default InputForm;
