0f1a545e3700d2d9b642791aa96853d72894701d
Abhay Baiju <abhay0198@gmail.com>
2020-10-14 16:55:31 +0530

    Created Sidedrawer

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -23,9 +23,7 @@ const AboutUs = () => {
           <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" fill="url(#grad1)"></path>
         </svg>
       </div>
-      <Grid>
         <Navbar/>
-      </Grid>
       <Grid container justify="space-evenly" alignItems="center" style={{marginTop:'8%', position:'sticky'}}>
         <Grid item lg={5} style={{color:'#3f3d56', marginLeft:'3%', marginRight:'-15%'}}>
           <Fade duration={1000} bottom>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Grid, AppBar, Toolbar, Typography, makeStyles} from '@material-ui/core';
+import {Grid, AppBar, Toolbar, Typography, makeStyles, Hidden} from '@material-ui/core';
 //import './css/Navbar.css';
 import {Link} from 'react-router-dom';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import SideDrawer from "./SideDrawer.js";
 
 
 //317e96
@@ -17,7 +18,7 @@ const Navbar = () => {
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.standard
       }),
-      padding: '0 20px',
+      padding: '0 0',
     },
     appBarScrolled: {
       backgroundColor: 'rgba(248,250,249,0.9)',
@@ -48,7 +49,7 @@ const Navbar = () => {
       background:'rgba(248,250,249,0.9)',
       borderRadius:'4px',
       textDecoration:'none',
-      padding:'6%',
+      padding:'4% 6%',
       "&:hover": {
         color:'white',
         border: '1px solid #3f3d56',
@@ -61,7 +62,7 @@ const Navbar = () => {
       background:'#d67477',
       borderRadius:'4px',
       textDecoration:'none',
-      padding:'6%',
+      padding:'4% 6%',
       "&:hover": {
         color:'#f2fcfe',
         border: '1px solid #3f3d56',
@@ -90,39 +91,47 @@ const Navbar = () => {
   });
 
   return(
-    <Grid>
-      <AppBar title="Dental-app" className={`${classes.appBar} ${
+      <AppBar lg={12} title="Dental-app" className={`${classes.appBar} ${
           trigger === false ? '' : classes.appBarScrolled
         }`}>
-        <Toolbar style={{ marginLeft:'10%', marginRight:'10%', marginTop:'1%'}}>
-          <Grid container justify="flex-start" alignItems="center">
+        <Toolbar style={{ marginLeft:'10%', marginRight:'0%', marginTop:'1%'}}>
+          <Grid container justify="flex-start" alignItems="center" lg={6} md={6} sm={6} xs={6}>
             <Typography>
               <Link to="/"><img alt="..." src={require("./images/logo.png")} height="50" style={{transform: 'rotate(-90deg)', cursor:'pointer'}}/></Link>
             </Typography>
-            <Typography style={{marginLeft:'3%', fontWeight:'bold', color:'grey'}}>
+            <Hidden xsDown>
+            <Typography style={{marginLeft:'3%', fontWeight:'bold', color:'#3f3d56'}}>
                 LILA DENTAL CLINIC
             </Typography>
+            </Hidden>
           </Grid>
-          <Grid container justify="space-evenly" alignItems="flex-right" spacing={3}>
-              <Grid item lg={1.5}>
-                <a href="/" title="Home" className={`${classes.a} ${trigger === false ? '' : classes.aScrolled}`}>Home</a>
-              </Grid>
-              <Grid item lg={2}>
-                <a href="/About" title="About us" className={`${classes.a} ${trigger === false ? '' : classes.aScrolled}`}>About Us</a>
-              </Grid>
-              <Grid item lg={2}>
-                <a href="/Services" title="Services" className={`${classes.a} ${trigger === false ? '' : classes.aScrolled}`}>Services</a>
-              </Grid>
-              <Grid item lg={2.5}>
-                <a href="/Contact" title="Contact Us" className={`${classes.a} ${trigger === false ? '' : classes.aScrolled}`}>Contact Us</a>
-              </Grid>
-              <Grid item lg={4}>
-                <a href="/Book" title="Book now" className={`${classes.btn} ${trigger === false ? '' : classes.btnScrolled}`}>Book appointment</a>
-              </Grid>
-          </Grid>
+          <Hidden smDown>
+            <Grid container justify="space-evenly" alignItems="flex-right" spacing={3} lg={12} md={12}>
+                <Grid item >
+                  <a href="/" title="Home" className={`${classes.a} ${trigger === false ? '' : classes.aScrolled}`}>Home</a>
+                </Grid>
+                <Grid item >
+                  <a href="/About" title="About us" className={`${classes.a} ${trigger === false ? '' : classes.aScrolled}`}>About Us</a>
+                </Grid>
+                <Grid item >
+                  <a href="/Services" title="Services" className={`${classes.a} ${trigger === false ? '' : classes.aScrolled}`}>Services</a>
+                </Grid>
+                <Grid item >
+                  <a href="/Contact" title="Contact Us" className={`${classes.a} ${trigger === false ? '' : classes.aScrolled}`}>Contact Us</a>
+                </Grid>
+                <Grid item lg={4} md={4}>
+                  <a href="/Book" title="Book now" className={`${classes.btn} ${trigger === false ? '' : classes.btnScrolled}`}>Book Now</a>
+                </Grid>
+            </Grid>
+          </Hidden>
+          <Hidden mdUp>
+          <Grid item sm={4} xs={4}>
+                  <a href="/Book" title="Book now" className={`${classes.btn} ${trigger === false ? '' : classes.btnScrolled}`}>Book Now</a>
+                </Grid>
+            <SideDrawer />
+          </Hidden>
         </Toolbar>
       </AppBar>
-    </Grid>
   );
 }
 
