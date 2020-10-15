f4e369932d86bc60358d7bfc09eee96b16f25515
Shambhavi <sarin.shambhavi@gmail.com>
2020-10-16 03:11:23 +0530

    Work

--- frontend/src/components/Work.js
+++ frontend/src/components/Work.js
@@ -1,6 +1,6 @@
 //Success colors and rating experience
 //FrontPage colors
-//Counter placement?✅
+//Counter placement?✅------timer? shift up??
 //Pics about
 //Leftover Content---Decide services and illustrations/images accordingly.----Write Reviews
 //Gridding
@@ -12,7 +12,6 @@
 //Razorpay live mode
 //Animations for book and about counter-----------done? Cards hover animation??
 //SES Live Mode
-//Validation Regex ✅------
 //separate dispatch?--------MAJOR ISSUE FACED!!!!!!!!
-//Configure env file✅------mail it to me!!!!!!!!
+//Configure env file✅
 //disable passed time on current date, disable date when all slots booked/last slot time passed
