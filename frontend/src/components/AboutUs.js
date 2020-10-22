1fd89b7bc1e62a61ec5fc1a21782e79dd36758e0
Shambhavi <sarin.shambhavi@gmail.com>
2020-10-23 02:28:38 +0530

    (#67) Counter Responsive, svgs left

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -8,6 +8,8 @@ import bg2 from './images/bg2.svg';
 import bg from './images/circled-background.svg';
 import about_bg from './images/about_bg.svg';
 import journey from './images/journey_bg.svg';
+import purple from './images/purpleCard.svg';
+import orange from './images/orangeCard.svg';
 import './css/AboutUs.css';
 import Fade from 'react-reveal/Fade';
 import about from './images/about.svg';
@@ -72,62 +74,50 @@ const AboutUs = () => {
             <p style={{ lineHeight:'1.8', color:'#404040', width:'100%', textAlign:'center', float:'right', fontSize:'18px'}}>Keep track of what's happening with your data, change permissions, and run reports against your data anywhere in the world.</p>
           </Fade>
         </Grid>
-        <Grid spacing={1} item xs={12} container justify="center" alignItems="stretch">
+        <Grid item container justify="center" alignItems="stretch" spacing={2} style={{marginBottom:0, paddingBottom:0}}>
           <Grid item lg={5} md={5} sm={6} xs={12}>
             <Fade duration={1000} bottom>
-              <p style={{marginLeft:'16%', marginRight:'6%', marginTop:'5%', lineHeight:'1.7', textAlign:'justify', color:'#404040'}}>
+              <p style={{marginLeft:'16%', marginRight:'6%', marginTop:'0%', lineHeight:'1.8', textAlign:'justify', color:'#404040'}}>
                 <h3 style={{color:'#645ba4', textAlign:'center', marginBottom:'-1%'}}>Dr C.S Baiju</h3>
                 <br></br>
                 Graduated in 1996 with an M.D.S degree, Dr Baiju is now one of the leading periodontists in Delhi. His thirst for knowledge is almost equal to his expertise in the ins and outs of dentistry. When not in his clinic, he can often be found chilling with his family or poring over a dental journal.
               </p>
             </Fade>
           </Grid>
-          <Grid item lg={5} md={5} sm={6} xs={10}>
+          <Grid item lg={5} md={5} sm={6} xs={10} style={{background:`url(${purple})`, backgroundSize:'cover', backgroundRepeat:'no-repeat', marginLeft:'12%'}}>
             <Fade duration={1000} bottom>
-              <Card style={{width:'80%', marginLeft:'10%', borderRadius:'0px', boxShadow:'none', border:'none'}}>
-                <CardContent style={{background:'#efeef6', marginTop:'3%'}}> {/*e0deed*/}
-                  <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" height="250" width="600" style={{marginTop:'-3%', marginBottom:'-6%', marginLeft:'-3.2%'}}>
-                    <circle fill="#e0deed" cx="-170" cy="-200" r="400"/>
-                  </svg>
-                  <img src={require('./images/CSBaiju.png')} alt="" height="245" style={{float:'left', marginTop:'-51%', position:'sticky', marginLeft:'-3%'}}/>
-                </CardContent>
-              </Card>
-            </Fade>
-          </Grid>
-        </Grid>
-        <Grid spacing={1} item xs={12} container justify="center" alignItems="stretch">
-          <Grid item lg={5} md={5} sm={6} xs={10} style={{marginTop:'4%'}}>
-            <Fade duration={1000} bottom>
-              <Card style={{width:'80%',borderRadius:'0px', boxShadow:'none', border:'none', marginLeft:'15%'}}>
-                <CardContent style={{background:'#ffebe6', marginTop:'3%'}}>{/*ffd6cc*/}
-                  <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" height="250" width="600" style={{marginTop:'-3%', marginBottom:'-6%', marginLeft:'-3.2%'}}>
-                    <circle fill="#ffd6cc" cx="250" cy="-200" r="400"/>
-                  </svg>
-                  <img src={require('./images/AnuradhaBaiju.png')} alt="" height="298" style={{float:'right', marginTop:'-62.5%', position:'sticky', marginRight:'-5%'}}/>
-                </CardContent>
-              </Card>
-            </Fade>
-          </Grid>
-          <Grid item lg={5} md={5} sm={6} xs={12} style={{marginTop:'2%'}}>
-            <Fade duration={1000} bottom>
-              <p style={{marginLeft:'12%', marginRight:'11%', marginTop:'8%', lineHeight:'1.8', textAlign:'justify', color:'#404040'}}>
-                <h3 style={{color:'#ff8566', textAlign:'center', marginBottom:'-1%'}}>Dr Anuradha Baiju</h3>
-                <br></br>
-                Dr Anuradha graduated in 1994 with a B.D.S degree and is now known for her patient-centric yet highly professional brand of dentistry. Her optimism is contagious and compliments her vast repertoire of dental knowledge. When not in her clinic, she can be found bonding with her two sons or bingeing on her favourite shows.
-              </p>
+              <img src={require('./images/CSBaiju.png')} alt="" height="253" style={{float:'left', marginTop:'-1%', marginLeft:'-1%', marginBottom:'-1%'}}/>
             </Fade>
           </Grid>
         </Grid>
       </Grid>
-      <Grid container justify="center" alignItems="center" style={{background:`url(${bg})`, backgroundRepeat:'no-repeat', backgroundSize:'cover', padding:'5% 0%', marginTop:'8%'}}>
+      <Grid container justify="center" alignItems="stretch" style={{marginTop:'5%'}}>
+        <Grid item lg={5} md={6} sm={8} xs={12} style={{marginLeft:'0%'}}>
+          <Fade duration={1000} bottom>
+            <div item style={{width:'75%', maxHeight:'100%', background:`url(${orange})`, backgroundSize:'cover', backgroundRepeat:'no-repeat', textAlign:'right', paddingRight:'35%'}}>
+              <img src={require('./images/AnuradhaBaiju.png')} alt="" height="300vh" style={{margin:'auto'}}/>
+            </div>
+          </Fade>
+        </Grid>
+        <Grid item lg={5} md={8} sm={10} xs={12} style={{marginTop:'2.5%'}}>
+          <Fade duration={1000} bottom>
+            <p style={{marginLeft:'11%', marginRight:'13%', marginTop:'0%', lineHeight:'1.8', textAlign:'justify', color:'#404040'}}>
+              <h3 style={{color:'#ff8566', textAlign:'center', marginBottom:'-1%'}}>Dr Anuradha Baiju</h3>
+              <br></br>
+              Dr Anuradha graduated in 1994 with a B.D.S degree and is now known for her patient-centric yet highly professional brand of dentistry. Her optimism is contagious and compliments her vast repertoire of dental knowledge. When not in her clinic, she can be found bonding with her two sons or bingeing on her favourite shows.
+            </p>
+          </Fade>
+        </Grid>
+      </Grid>
+      <Grid container justify="center" alignItems="flex-start" style={{background:`url(${bg})`, backgroundRepeat:'no-repeat', backgroundSize:'cover', padding:'5% 0%', marginTop:'8%'}}>
         <Card style={{width:'75%'}} classes={{root:classes.card, hover:classes.hover}}>
-          <CardContent style={{padding:'5%', marginBottom:'0%'}}>
-            <Grid justify="center" alignItems="stretch" container item lg={12} xs={12} style={{padding:'5% 2%'}}>
-                <Grid item lg={5} xs={5}>
-                  <img src={require('./images/speaker.svg')} alt="" style={{float:'left', marginRight:'7.4%', marginTop:'-10%', marginBottom:'0%', width: '80%'}}/>
+          <CardContent style={{padding:'3%', marginBottom:'0%'}}>
+            <Grid justify="center" alignItems="stretch" container item lg={12} xs={12} style={{padding:'3% 1%'}}>
+                <Grid item lg={5} md={8} sm={8} xs={12}>
+                  <img src={require('./images/speaker.svg')} alt="" style={{float:'left', marginRight:'7.4%', marginTop:'-5%', marginBottom:'0%', width: '80%'}}/>
                 </Grid>
-                <Grid item lg={7} xs={7}>
-                  <p style={{color:'#3f3d56', fontWeight:'600', fontSize:'2.3vw', marginTop:'-1%'}}>Still not sure? Let us change your mind</p>
+                <Grid item lg={7} xs={12}>
+                  <p style={{color:'#3f3d56', fontWeight:'600', fontSize:'3.5vh', marginTop:'3%', textAlign:'center'}}>Still not sure? Let us change your mind</p>
                   <Table style={{width:'100%'}}>
                     <TableRow>
                       <TableCell style={{border:'none', fontSize:'28px', color:'#3f51b5', width:'33%', padding:'4px 8px', textAlign:'center'}}><CountUp start={0} end={1000} duration={2} suffix="+" useEasing={false} redraw={true}></CountUp></TableCell>
@@ -141,8 +131,8 @@ const AboutUs = () => {
                     </TableRow>
                   </Table>
                 </Grid>
-                <Grid item lg={12} md={12} sm={12} xs={12}>
-                  <Link to="/Book" style={{textDecoration:'none'}}><Button style={{color:'white', marginLeft:'50%',marginTop:'4%', marginBottom:'-5.5%',background:'#645ba4', padding:'0.5vw 3%', width:'15vw'}}>Book now</Button></Link>
+                <Grid item lg={12} md={12} sm={12} xs={12} style={{textAlign:'center'}}>
+                  <Link to="/Book" style={{textDecoration:'none'}}><Button style={{color:'white', marginLeft:'0%',marginTop:'2%', marginBottom:'-1.5%',background:'#645ba4', padding:'2% 3%', width:'30%'}}>Book now</Button></Link>
                 </Grid>
             </Grid>
           </CardContent>
